57f4469ff3c3055886f297e32cac0303d9578bd7
MÁRCIO MILET DE CARVALHO NEVES JUNIOR <MRCIO5445@IOT>
2024-10-19 11:09:32 -0300

    creating migrations

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import *
+
+@admin.register(Empresa)
+
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ('cnpj','name','razao_social','sede','estado','ranking')
+
+
+@admin.register(Area_cobertura)
+
+class AreaCoberturaAdmin(admin.ModelAdmin):
+    list_display = ('id','tecnologias')
+
+
+@admin.register(empresa_area_cobertura)
+
+class empresaAreaCobertura(admin.ModelAdmin):
+    list_display = ('empresa_cnpj', 'area_cobertura_id')
+
+
+@admin.register(Plano)
+
+class Plano(admin.ModelAdmin):
+    list_display = ('id', 'banda', 'empresa_cnpj')
+
+
+@admin.register(Infraestrutura)
+
+class Infuraestrutura(admin.ModelAdmin):
+    list_display = ('id', 'asn', 'ptt', 'bgp', 'operadora_backbone', 'capacidade_backbone', 'empresa_cnpj')
+
+
+
+
 
 # Register your models here.
--- backend/models.py
+++ backend/models.py
@@ -1,82 +1,88 @@
 from django.db import models
 
-class Empresa():
-    cnpj = models.CharField(max_length = 18, unique = True)
+class Empresa(models.Model):
+    cnpj = models.CharField(max_length = 18, unique = True, primary_key=True)
     name = models.CharField(max_length = 45)
     razao_social = models.CharField(max_length = 45)
     sede = models.CharField(max_length = 45)
     estado = models.CharField(max_length = 45)
     ranking = models.CharField(max_length = 45)
 
-class Area_cobertura():
-    id = models.IntegerField
+    class meta():
+        verbose_name = 'empresa'
+        verbose_name_plural = 'empresas'
+    def __str__(self):
+        return self.cnpj
+
+class Area_cobertura(models.Model):
+    id = models.IntegerField(unique = True, primary_key=True)
     tecnologias = models.CharField(max_length = 45)
 
-class empresa_area_cobertura():
-    empresa_cnpj = models.ForeignKey(Empresa, related_name='cnpj')
-    area_cobertura_id = models.ForeignKey(Area_cobertura)
+class empresa_area_cobertura(models.Model):
+    empresa_cnpj = models.ForeignKey(Empresa, related_name='area_empresa_cnpj', on_delete= models.CASCADE)
+    area_cobertura_id = models.ForeignKey(Area_cobertura, related_name='area_cobertura_id', on_delete=models.CASCADE)
 
-class Plano():
-    id = models.IntegerField()
+class Plano(models.Model):
+    id = models.IntegerField(unique = True ,primary_key=True)
     banda = models.CharField(max_length = 45)
-    empresa_cnpj = models.ForeignKey(Empresa, related_name='cnpj')
+    empresa_cnpj = models.ForeignKey(Empresa, related_name='plano_empresa_cnpj', on_delete= models.CASCADE)
 
-class Infraestrutura():
-    id = models.IntegerField()
+class Infraestrutura(models.Model):
+    id = models.IntegerField(unique = True, primary_key=True)
     asn = models.CharField(max_length = 45)
     ptt = models.CharField(max_length = 45)
     bgp = models.CharField(max_length = 45)
     operadora_backbone = models.CharField(max_length = 45)
     capacidade_backbone = models.CharField(max_length = 45)
-    empresa_cnpj = models.ForeignKey(Empresa, related_name = 'cnpj')
+    empresa_cnpj = models.ForeignKey(Empresa, related_name = 'infra_empresa_cnpj', on_delete= models.CASCADE)
 
-class Servico():
-    id = models.IntegerField()
+class Servico(models.Model):
+    id = models.IntegerField(unique = True, primary_key=True)
     tipo_plano = models.CharField(max_length = 45)
     sla = models.CharField(max_length = 45)
     preco = models.IntegerField()
-    empresa_cnpj = models.ForeignKey(Empresa, related_name = 'cnpj')
-    plano_id = models.ForeignKey(Plano, related_name = 'id')
-    plano_empresa_cnpj = models.ForeignKey(Plano, related_name='empresa_cnpj')
+    empresa_cnpj = models.ForeignKey(Empresa, related_name = 'servico_empresa_cnpj', on_delete= models.CASCADE)
+    plano_id = models.ForeignKey(Plano, related_name = 'servico_plano_id', on_delete= models.CASCADE)
+    plano_empresa_cnpj = models.ForeignKey(Plano, related_name='servico_plano_empresa_cnpj', on_delete= models.CASCADE)
 
-class Setor():
-    id = models.IntegerField()
+class Setor(models.Model):
+    id = models.IntegerField(unique = True, primary_key=True)
     nome = models.CharField(max_length = 45)
 
-class Pessoa():
-    cpf = models.CharField(max_length = 45)
+class Pessoa(models.Model):
+    cpf = models.CharField(max_length = 45, unique= True, primary_key=True)
     nome = models.CharField(max_length = 45)
     cargo = models.CharField(max_length = 45)
     email = models.CharField(max_length = 45)
     telefone = models.CharField(max_length = 45)
-    setor_id = models.ForeignKey(Setor, related_name = 'id') 
+    setor_id = models.ForeignKey(Setor, related_name = 'pessoa_setor_id', on_delete= models.CASCADE) 
 
-class Endereco():
-    id = models.IntegerField()
+class Endereco(models.Model):
+    id = models.IntegerField(unique = True, primary_key=True)
     estado = models.CharField(max_length = 45)
     cidade = models.CharField(max_length = 45)
     bairro = models.CharField(max_length = 45)
-    empresa_cnpj = models.ForeignKey(Empresa, related_name='cnpj')
-    pessoa_cpf = models.ForeignKey(Pessoa, related_name='cpf')
+    empresa_cnpj = models.ForeignKey(Empresa, related_name='endereco_empresa_cnpj', on_delete= models.CASCADE)
+    pessoa_cpf = models.ForeignKey(Pessoa, related_name='endereco_pessoa_cpf', on_delete= models.CASCADE)
 
 
-class Servidor(models.Model):
-    name = models.CharField()
-    class meta():
-        abstract = True
+# class Servidor(models.Model):
+#     name = models.CharField()
+#     class meta():
+#         abstract = True
 
-class Backbone(Servidor):
-    provedor = models.TextField()
+# class Backbone(Servidor):
+#     provedor = models.TextField()
 
-    class Meta():
-        verbose_name = 'backbone'
-        verbose_name_plural = 'backbones'
+#     class Meta():
+#         verbose_name = 'backbone'
+#         verbose_name_plural = 'backbones'
 
-    def __str__(self):
-        return self.provedor
+#     def __str__(self):
+#         return self.provedor
 
-class Dns(Servidor):
-    class Meta():
-        verbose_name = 'dns'
-        verbose_name_plural = 'dns'
+# class Dns(Servidor):
+#     class Meta():
+#         verbose_name = 'dns'
+#         verbose_name_plural = 'dns'
 # Create your models here.
